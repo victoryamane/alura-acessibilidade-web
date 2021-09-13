83163c0b6ea744c1b556f69c4a6a2ac496dcc977
Victor Yamane <yamanevictor@gmail.com>
2021-09-13 11:37:26 -0300

    Curso Acessibilidadade parte 2 - Aula 01

---
+++ js/dialog.js
@@ -0,0 +1,22 @@
+// Variáveis
+var btnAbreDialog = document.querySelector('#abreDialog');
+var dialog = document.querySelector('.dialogNewsletter');
+var dialogBody = document.querySelector('.dialogNewsletter-body');
+var dialogOverlay = document.querySelector('.dialogNewsletter-overlay');
+
+btnAbreDialog.style.display = 'block';
+
+
+// Quando abrir a dialog...
+btnAbreDialog.addEventListener('click', function () {
+    dialog.classList.add('dialogNewsletter--aberto');
+    document.querySelector('.dialogNewsletter-campo').focus();
+});
+
+function fechandoDialog() {
+    document.activeElement.blur();
+    dialog.classList.remove('dialogNewsletter--aberto');
+}
+
+// Listeners
+document.querySelector('.dialogNewsletter-fechar').addEventListener('click', fechandoDialog);
